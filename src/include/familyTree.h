13e795da2ac50f566ca5bf699621516c71535237
Luiz Gustavo Monteiro <luizgu_mreis@hotmail.com>
2025-04-30 17:33:20 -0300

    Added Struct Family Tree

---
+++ src/include/familyTree.h
@@ -0,0 +1,17 @@
+#ifndef FAMILY_TREE_H // include protection.
+#define FAMILY_TREE_H
+
+typedef struct FamilyTree
+{
+    int id;              /* Unique identifier.*/
+    char firstName[100]; /* Firt Name of Person */
+    char middleName[100];
+    char lastName[100];
+    int age;                      /* Age of person */
+    struct FamilyTree *father;    /* Pointer to father, the first must be initate as NULL */
+    struct FamilyTree **children; /* Pointer to Array of children */
+    char specialDescription[300]; /* Special description about person 299 + \0 */
+    int childrenCount;            /* Children quantity */
+} FamilyTree;
+
+#endif
